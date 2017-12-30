437733af7ed4139a7e635cb0c3906e0c5746f318
Jérémie Zarca <jzarca01@users.noreply.github.com>
2017-12-30 15:11:06 +0100

    Update buster.py

--- buster/buster.py
+++ buster/buster.py
@@ -102,12 +102,12 @@ def main():
                 return d.html(method='html').encode('utf8')
             return d.__unicode__().encode('utf8')
         
-        def fix_meta_links(text,parser):
+        def fix_meta_image_links(text,parser):
             td_regex = re.compile(target_domain + '|' )
             
             assert target_domain, "target domain must be specified --target_domain=<http://your-host-url>"
             d = PyQuery(bytes(bytearray(text, encoding='utf-8')), parser=parser)
-            for share_class in ['meta[property="og:image"], meta[property="twitter:image"]']:
+            for share_class in ['meta[property="og:image"], meta[name="twitter:image"]']:
                 print "share_class : ", share_class
                 for element in d(share_class):
                     e = PyQuery(element)
@@ -118,7 +118,28 @@ def main():
                     content_target_domain = target_domain.replace("/static", "")
                     print "target_domain : ", content_target_domain
                     new_href = re.sub(domain, content_target_domain, href)
-                    e.attr('href', new_href)
+                    e.attr('content', new_href)
+                    print "\t", href, "=>", new_href
+            if parser == 'html':
+                return d.html(method='html').encode('utf8')
+            return d.__unicode__().encode('utf8')
+        
+        def fix_meta_url_links(text,parser):
+            td_regex = re.compile(target_domain + '|' )
+            
+            assert target_domain, "target domain must be specified --target_domain=<http://your-host-url>"
+            d = PyQuery(bytes(bytearray(text, encoding='utf-8')), parser=parser)
+            for share_class in ['meta[property="og:url"], meta[name="twitter:url"]']:
+                print "share_class : ", share_class
+                for element in d(share_class):
+                    e = PyQuery(element)
+                    print "element : ", e
+                    href = e.attr('content')
+                    print "href : ", href
+                    print "domain : ", domain
+                    print "target_domain : ", target_domain
+                    new_href = re.sub(domain, target_domain, href)
+                    e.attr('content', new_href)
                     print "\t", href, "=>", new_href
             if parser == 'html':
                 return d.html(method='html').encode('utf8')
@@ -139,7 +160,8 @@ def main():
                 print "fixing links in ", filepath
                 newtext = fixLinks(filetext, parser)
                 newtext = fix_share_links(filetext,parser)
-                newtext = fix_meta_links(filetext,parser)
+                newtext = fix_meta_image_links(filetext,parser)
+                newtext = fix_meta_url_links(filetext,parser)
                 with open(filepath, 'w') as f:
                     f.write(newtext)
 
